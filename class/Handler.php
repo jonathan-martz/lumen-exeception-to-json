861e582297d65640f5b4fba5a73bc4e5f414bebd
jonathan <bitbucket@jmartz.de>
2019-08-25 12:27:25 +0200

    add message auf queryExeception

--- class/Handler.php
+++ class/Handler.php
@@ -1,28 +1,31 @@
 <?php
-	namespace MkaaaaaaaY\LumenExecptionToJson;
+namespace MkaaaaaaaY\LumenExecptionToJson;
 
-	use \Illuminate\Http\Request;
-	use \Exception;
-	use App\Http\Controllers\Controller;
+use \Illuminate\Http\Request;
+use \Exception;
+use App\Http\Controllers\Controller;
 
-	class Handler extends Controller{
+class Handler extends Controller{
 
-		public function toJson(Request $request, $e){
-			$message = 'Unknown Exeception: '.get_class($e);
+    public function toJson(Request $request, $e){
+        $message = 'Unknown Exeception: '.get_class($e);
 
-			if(get_class($e) == 'Symfony\Component\HttpKernel\Exception\NotFoundHttpException'){
-				$message = 'Route not Found.';
-			}
-			else if(get_class($e) == 'Illuminate\Validation\ValidationException'){
-				$message = $e->getMessage();
-				$this->addResult('validation',$e->errors());
-			}
-			else{
-				$message = 'Unknown Exeception: '.get_class($e);
-			}
+        if(get_class($e) == 'Symfony\Component\HttpKernel\Exception\NotFoundHttpException'){
+            $message = 'Route not Found.';
+        }
+        else if(get_class($e) == 'Illuminate\Validation\ValidationException'){
+            $message = $e->getMessage();
+            $this->addResult('validation',$e->errors());
+        }
+        else if(get_class($e) == 'Illuminate\Database\QueryException'){
+            $message = $e->getMessage();
+        }
+        else{
+            $message = 'Unknown Exeception: '.get_class($e);
+        }
 
-			$this->addMessage('exception',$message);
+        $this->addMessage('exception',$message);
 
-			return $this->getResponse();
-		}
-	}
+        return $this->getResponse();
+    }
+}
