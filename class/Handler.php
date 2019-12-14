84311e63e7ac7491888a9372a7c78f10d701a398
jonathan-martz <github@jmartz.de>
2019-12-14 21:34:07 +0100

    refactor class, add debug switch

--- class/Handler.php
+++ class/Handler.php
@@ -1,28 +1,30 @@
 <?php
-	namespace MkaaaaaaaY\LumenExecptionToJson;
+namespace MkaaaaaaaY\LumenExecptionToJson;
 
-	use \Illuminate\Http\Request;
-	use \Exception;
-	use App\Http\Controllers\Controller;
+use \Illuminate\Http\Request;
+use \Exception;
+use App\Http\Controllers\Controller;
 
-	class Handler extends Controller{
+class Handler extends Controller{
 
-		public function toJson(Request $request, $e){
-			$message = 'Unknown Exeception: '.get_class($e);
+    public function toJson(Request $request, $e){
+        $message = 'Unknown Exeception: '.get_class($e);
 
-			if(get_class($e) == 'Symfony\Component\HttpKernel\Exception\NotFoundHttpException'){
-				$message = 'Route not Found.';
-			}
-			else if(get_class($e) == 'Illuminate\Validation\ValidationException'){
-				$message = $e->getMessage();
-				$this->addResult('validation',$e->errors());
-			}
-			else{
-				$message = 'Unknown Exeception: '.$e->getMessage();
-			}
+        if(get_class($e) == 'Illuminate\Validation\ValidationException'){
+            $message = $e->getMessage();
+            $this->addResult('validation',$e->errors());
+        }
+        else{
+            $message = 'Unknown Exeception: '.$e->getMessage();
+        }
 
-			$this->addMessage('exception',$message);
+        $this->addMessage('exception',$message);
 
-			return $this->getResponse();
-		}
-	}
+        if($request->cookie('debug') === 'true'){
+            $this->addMessage('file',$e->getFile());
+            $this->addMessage('line',$e->getLine());
+        }
+
+        return $this->getResponse();
+    }
+}
